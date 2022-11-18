1e95c752f2ddeb7e058d632238355ff47c85ed94
Alexey Oplachko <xirenec1@gmail.com>
2022-11-18 13:27:26 +0200

    Cleanup

--- src/helpers/org.interceptor.js
+++ src/helpers/org.interceptor.js
@@ -1,14 +1,10 @@
 import store from "../store/store";
-import { errorHandler, createAlert } from "../actions";
-import setApiWarning from "../actions/setApiWarning";
-import { AxiosInstance } from "axios";
 const orgInterceptor = (axiosInstance) => {
     // Add a request interceptor
     axiosInstance.interceptors.request.use(
         (config) => {
             // Do something before request is sent
             const { org } = store.getState();
-            console.log(org)
             if (org) {
                 config.headers["X-Scope-OrgID"] = org;
             }
