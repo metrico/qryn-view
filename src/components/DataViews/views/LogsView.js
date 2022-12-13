a3a98c4bf6930a620c1d0caecb7ad6bcfcd72f1e
jacovinus <jacovinus@gmail.com>
2022-12-13 18:04:45 +0100

    feat: Use tabs for different vis types #127

--- src/components/DataViews/views/LogsView.js
+++ src/components/DataViews/views/LogsView.js
@@ -1,38 +1,65 @@
+import { TabsUnstyled } from "@mui/base";
+
 import { LogRows } from "../components/Logs/LogRows";
 import { VectorTable } from "../components/Table/VectorTable/VectorTable";
 import { ViewHeader } from "../components/ViewHeader";
-import { ViewStyled } from "./styled";
+import { TabsList, Tab, TabPanel, ViewStyled } from "./styled";
 
-     
-     export default function LogsView(props){
+export default function LogsView(props) {
+    const {
+        viewRef,
+        panelSize,
+        viewHeight,
+        setStreamClose,
+        setMaxHeight,
+        setMinimize,
+        actualQuery,
+        total,
+        type,
+        theight,
+        tableData,
+        streamData,
+    } = props;
 
-        const {
-            viewRef,
-            panelSize,
-            viewHeight,
-            setStreamClose,
-            setMaxHeight,
-            setMinimize,
-            actualQuery,
-            total,
-            type,
-            theight,
-            tableData,
-            streamData
-        } = props
+    return (
+        <ViewStyled ref={viewRef} size={panelSize} vheight={viewHeight}>
+            <ViewHeader
+                onClose={setStreamClose}
+                setMinimize={setMinimize}
+                setMaxHeight={setMaxHeight}
+                actualQuery={actualQuery}
+                total={total}
+                type={type}
+                {...props}
+            />
 
-        return (
-            <ViewStyled ref={viewRef} size={panelSize} vheight={viewHeight}>
-                <ViewHeader
-                    onClose={setStreamClose}
-                    setMinimize={setMinimize}
-                    setMaxHeight={setMaxHeight}
-                    actualQuery={actualQuery}
-                    total={total}
-                    type={type}
-                    {...props}
-                />
-                <div className="view-content">
+            <TabsUnstyled defaultValue={0}>
+                <TabsList panelsize={props.panelSize}>
+                    <Tab>Logs</Tab>
+                    <Tab>Table</Tab>
+                </TabsList>
+                <TabPanel value={0}>
+                    <div className="view-content">
+                        <LogRows
+                            {...props}
+                            onClose={setStreamClose}
+                            messages={streamData}
+                            actualQuery={actualQuery}
+                        />
+                    </div>
+                </TabPanel>
+                <TabPanel value={1}>
+                    <div className="view-content">
+                        <VectorTable
+                            {...props}
+                            height={theight}
+                            data={tableData}
+                            actualQuery={actualQuery}
+                        />
+                    </div>
+                </TabPanel>
+            </TabsUnstyled>
+            {/* <div className="view-content">
                     {actualQuery?.tableView ? (
                         <VectorTable
                             {...props}
@@ -48,7 +75,7 @@ import { ViewStyled } from "./styled";
                             actualQuery={actualQuery}
                         />
                     )}
-                </div>
-            </ViewStyled>
-        );
-     }  
+                </div> */}
+        </ViewStyled>
+    );
+}
