985babe4fc12af6c208d5d89a5f62e2cc81d435d
jacovinus <jacovinus@gmail.com>
2022-12-15 18:27:02 +0100

    feat: #160 and #161

--- src/index.js
+++ src/index.js
@@ -12,11 +12,12 @@ import errorInterceptor from './helpers/error.interceptor';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import DataSources from './views/DataSources/DataSources';
-
+import {CookiesProvider} from 'react-cookie'
 errorInterceptor(axios)
 
 ReactDOM.render(
   <React.StrictMode>
+<CookiesProvider>
      <Provider store={store}>
     <BrowserRouter>
       <Routes>
@@ -27,6 +28,7 @@ ReactDOM.render(
       </Routes>
     </BrowserRouter>
     </Provider>
+    </CookiesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
