1e614ffceeb70cab5adba50adc1c8efaf427e558
jacovinus <jacovinus@gmail.com>
2022-12-27 17:34:33 +0100

    feat:custom select

--- src/components/StatusBar/components/copylinkbutton/CopyLinkButton.js
+++ src/components/StatusBar/components/copylinkbutton/CopyLinkButton.js
@@ -4,8 +4,15 @@ import setLinksHistory from "../../../../actions/setLinksHistory";
 import { setIsSubmit } from "../../../../actions/setIsSubmit";
 import { DatePickerButton, UrlCopyButton } from "../../styled";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
-import { Checkbox, FormControlLabel, FormGroup, MenuItem, Tooltip, Typography } from "@mui/material";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import {
+    Checkbox,
+    FormControlLabel,
+    FormGroup,
+    MenuItem,
+    Tooltip,
+    Typography,
+} from "@mui/material";
 import { storedUrl } from "./helpers";
 import { StyledMenu } from "../daterangepicker";
 import { useState } from "react";
@@ -14,7 +21,7 @@ import { useLocation } from "react-router-dom";
 
 export default function CopyLinkButton() {
     const LINK_COPIED = "Link Copied To Clipboard";
-  
+
     const dispatch = useDispatch();
     const storeTheme = useSelector((store) => store.theme);
     const qrynTheme = themes[storeTheme];
@@ -22,7 +29,7 @@ export default function CopyLinkButton() {
     const open = Boolean(anchorEl);
     const [isRelative, setIsRelative] = useState(false);
 
-    const label = useSelector(({label}) => label);
+    const label = useSelector(({ label }) => label);
     function alertSuccess() {
         dispatch(
             createAlert({
@@ -38,10 +45,8 @@ export default function CopyLinkButton() {
     function shareDefaultLink(copyText) {
         navigator.clipboard.writeText(copyText).then(
             function () {
-           
-                    storeHistory();
-                    alertSuccess();
-              
+                storeHistory();
+                alertSuccess();
             },
             function (err) {
                 console.log("error on copy", err);
@@ -65,9 +70,8 @@ export default function CopyLinkButton() {
         textArea.select();
 
         return new Promise((res, rej) => {
-           
-                storeHistory();
-          
+            storeHistory();
+
             alertSuccess();
             document.execCommand("copy") ? res() : rej();
             textArea.remove();
@@ -75,20 +79,18 @@ export default function CopyLinkButton() {
     }
     const setTitle = "Copy Link";
 
-    const setActive = true
+    const setActive = true;
 
     const { hash } = useLocation();
     function copyLink(e) {
         e.preventDefault();
         setSubmitted();
-        const params = new URLSearchParams(hash.replace("#", ""))
-        params.set('label', label)
-        for (let [key, value] of params.entries()) {
-            console.log(key, value)
-        }
+        const params = new URLSearchParams(hash.replace("#", ""));
+        params.set("label", label);
         const locationWithLabel = new URL(window.location.href);
         locationWithLabel.hash = `#${params.toString()}`;
-        const copyText = isRelative && label ? locationWithLabel : window.location.href;
+        const copyText =
+            isRelative && label ? locationWithLabel : window.location.href;
         setTimeout(() => {
             if (navigator?.clipboard && window.isSecureContext) {
                 shareDefaultLink(copyText);
@@ -101,73 +103,82 @@ export default function CopyLinkButton() {
         setAnchorEl(event.currentTarget);
         setIsRelative(isRelative && label);
     };
-    const handleClose = (e, direction,  option) => {
+    const handleClose = (e, direction, option) => {
         setAnchorEl(null);
     };
 
     const handleChange = (event) => {
         setIsRelative(event.target.checked);
     };
-  
+
     return (
         <>
             <Tooltip title={setTitle}>
                 <>
-                <UrlCopyButton attachedSide={'r'} onClick={copyLink} isActive={setActive}>
-                    <ContentCopyIcon fontSize={"15px"} />
+                    <UrlCopyButton
+                        attachedSide={"r"}
+                        onClick={copyLink}
+                        isActive={setActive}
+                    >
+                        <ContentCopyIcon fontSize={"15px"} />
 
-                    <span>{"Copy Link"}</span>
-                </UrlCopyButton>
-                <DatePickerButton attachedSide={'l'} 
-                    onClick={handleClick}
-                    size={'small'}
-                    className={"date-time-selector"}
-                    aria-controls={open ? 'backward-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    isActive={setActive}>
-                    <KeyboardArrowDownOutlinedIcon fontSize={"12px"} />
-                </DatePickerButton>
-                <StyledMenu
-                            id="backward-menu"
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                            qryntheme={qrynTheme}
-                            size={'small'}
+                        <span>{"Copy Link"}</span>
+                    </UrlCopyButton>
+                    <DatePickerButton
+                        attachedSide={"l"}
+                        onClick={handleClick}
+                        size={"small"}
+                        className={"date-time-selector"}
+                        aria-controls={open ? "backward-menu" : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? "true" : undefined}
+                        isActive={setActive}
+                    >
+                        <KeyboardArrowDownOutlinedIcon fontSize={"12px"} />
+                    </DatePickerButton>
+                    <StyledMenu
+                        id="backward-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        qryntheme={qrynTheme}
+                        size={"small"}
+                    >
+                        <MenuItem
+                            key={`relativeTime`}
+                            style={{ padding: "0 14px" }}
                         >
-                            <MenuItem
-                                key={`relativeTime`}
-                                style={{ padding: "0 14px" }}
-                            >
-                                <FormGroup>
-                                    <FormControlLabel
-                                        style={{
-                                            padding: "0",
-                                            marginRight: 0,
-                                            cursor: !label
-                                                ? "not-allowed"
-                                                : "default",
-                                        }}
-                                        checked={isRelative}
-                                        onChange={handleChange}
-                                        control={
-                                            <Checkbox
-                                                style={{ paddingRight: "0px" }}
-                                                disabled={!label}
-                                            />
-                                        }
-                                        label={
-                                            <Typography
-                                                style={{ fontSize: "12px", color: qrynTheme.textColor }}
-                                            >
-                                                Relative time
-                                            </Typography>
-                                        }
-                                    />
-                                </FormGroup>
-                            </MenuItem>
-                        </StyledMenu>
+                            <FormGroup>
+                                <FormControlLabel
+                                    style={{
+                                        padding: "0",
+                                        marginRight: 0,
+                                        cursor: !label
+                                            ? "not-allowed"
+                                            : "default",
+                                    }}
+                                    checked={isRelative}
+                                    onChange={handleChange}
+                                    control={
+                                        <Checkbox
+                                            style={{ paddingRight: "0px" }}
+                                            disabled={!label}
+                                        />
+                                    }
+                                    label={
+                                        <Typography
+                                            style={{
+                                                fontSize: "12px",
+                                                color: qrynTheme.textColor,
+                                            }}
+                                        >
+                                            Relative time
+                                        </Typography>
+                                    }
+                                />
+                            </FormGroup>
+                        </MenuItem>
+                    </StyledMenu>
                 </>
             </Tooltip>
         </>
