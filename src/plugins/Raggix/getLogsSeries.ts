f47d43a800ff2cd46212a19cb649bf100a5f83cf
jacovinus <jacovinus@gmail.com>
2023-03-29 21:21:35 +0200

    feat:raggix viz

---
+++ src/plugins/Raggix/getLogsSeries.ts
@@ -0,0 +1,115 @@
+function request<TResponse>(
+    url: string,
+
+    config: RequestInit = {}
+): Promise<TResponse> {
+    return fetch(url, config)
+        .then((response) => response.json())
+        .then((data) => data);
+}
+
+const getLabels = async (labelsUrl: string) => {
+    try {
+        let labelsData: any = await request(labelsUrl);
+        if (labelsData?.data?.length > 0) {
+            return labelsData.data;
+        }
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+};
+
+const getLogQueries = async (
+    logLabels: any[],
+    filterValues: any[],
+    labelValuesUrl: Function
+) => {
+    let logQueries = [];
+    try {
+        for (let item of logLabels) {
+            /* Filter Metric labels out */
+            if (filterValues.includes(item)) {
+                continue;
+            }
+            if (item !== "") {
+                let values: any = await request(labelValuesUrl(item));
+                for (let val of values?.data) {
+                    logQueries.push(`{${item}="${val}"}`);
+                }
+            }
+        }
+        return logQueries;
+    } catch (e) {
+        console.log(e);
+        return logQueries;
+    }
+};
+
+const getLogs = async (logQueries: any[], logRequest: Function) => {
+    let res: any = [];
+    try {
+        for (let query of logQueries) {
+            let logs: any = await request(logRequest(query));
+            if (logs?.data?.result?.length <= 0) {
+                continue;
+            }
+
+            res = res.concat(logs?.data?.result);
+        }
+
+        return res;
+    } catch (e) {
+        console.log(e);
+        return res;
+    }
+};
+
+const getLogsSeries = async (start: number, end: number, host: string) => {
+    let filterValues = [
+        "__name__",
+        "le",
+        "server",
+        "emitter",
+        "client",
+        "span_kind",
+        "span_name",
+        "status_code",
+    ];
+
+    let labelsUrl = `${host}/loki/api/v1/labels?start=${start * 1000000}&end=${
+        end * 1000000
+    }`;
+
+    let labelValuesUrl = (label: string) =>
+        `${host}/loki/api/v1/label/${label}/values?start=${
+            start * 1000000
+        }&end=${end * 1000000}`;
+
+    let logRequest = (query: string) =>
+        `${host}/loki/api/v1/query_range?query=${encodeURIComponent(
+            query
+        )}&start=${start * 1000000}&end=${end * 1000000}&limit=10`;
+
+    let logQueries: any = [];
+
+    let result: any = [];
+
+    let logLabels: any[] = await getLabels(labelsUrl);
+
+    if (logLabels?.length > 0) {
+        logQueries = await getLogQueries(
+            logLabels,
+            filterValues,
+            labelValuesUrl
+        );
+    }
+
+    if (logQueries.length > 0) {
+        result = await getLogs(logQueries, logRequest);
+    }
+
+    return result;
+};
+
+export default getLogsSeries;
--- src/plugins/Raggix/index.tsx
+++ src/plugins/Raggix/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
-import { useWorker } from "@koale/useworker";
-import getLogs from "./worker";
+
+import { Switch, Tooltip } from "@mui/material";
+import getLogsSeries from "./getLogsSeries";
 
 const useDataSources = () => {
     let dataSources = useSelector((store: any) => store.dataSources);
@@ -15,54 +16,168 @@ const useDataSources = () => {
     return dsSelect;
 };
 
-interface WorkerMessage {
-    start: number;
-    end: number;
-    host: string;
-}
 
-//   const worker = new Worker('./worker.tsx', { type: 'module' });
-
-//   const getLogsWorker = (start: number, end: number, url:string): Promise<any> => {
-//     return new Promise((resolve, reject) => {
-//       const message: WorkerMessage = { start, end, url };
-//       worker.postMessage(message);
-//       worker.onmessage = (e: MessageEvent) => {
-//         resolve(e.data);
-//       };
-//       worker.onerror = (e: ErrorEvent) => {
-//         reject(e);
-//       };
-//     });
-//   };
 
 const Raggix = (props: any) => {
     const ds = useDataSources();
-    const [logsWorker] = useWorker(getLogs,{timeout:5000});
-    // const [logs, setLogs] = useState<any>("");
-    const logsData = async (start:any, end:any, url:string) => {
-        console.log("loading data...")
-        let result = await logsWorker(start, end, url);
-        console.log(result,"END")
-        
 
+ 
+
+    const [logs, setLogs] = useState<any>("");
+    const [isRecurrent, setIsRecurrent] = useState(false);
+    const [index, setIndex] = useState(0)
+    const [open, setOpen] = useState(false)
+
+    const [actTimestamp, setActTimestamp] = useState(Date.now());
+
+    const handleRecurrent = (e: any) => {
+        setIsRecurrent(() => e.target.checked);
+    };
+    const handleReset = () => {
+        setLogs(() => []);
+    };
+
+    const openLog = (e:any) => {
+     console.log(e)
+     setIndex(()=>e)
+    }
+
+    const openLogs = (e:any) =>{
+
+        setOpen(()=>e.target.checked)
     }
 
     useEffect(() => {
-        const end = Date.now();
-        const start = end - 5000;
-           logsData(start,end,ds.Logs.url)
-    },[]);
+        // const interval = setInterval(() => {
+        //   setActTimestamp(Date.now());
+        // }, 30000);
+
+        // return () => clearInterval(interval);
+
+        if (isRecurrent) {
+            // do worker stuff
+            const end = Date.now();
+            const start = end - 5000;
+            const host = ds?.Logs?.url;
+            let res = getLogsSeries(start, end, host);
+            res.then((data) => {
+                setLogs((prev:any) =>{ 
+                if(prev?.length > 0 && data?.length > 0) {
+                    let cp = [...prev]
+                   return  [...cp, data] 
+                }
+
+                    return data
+                 });
+            });
+        }
+    }, [isRecurrent]);
+
+    // const logsData = async (start: any, end: any, url: string) => {
+    //     console.log("loading data...");
+    //     let result = await logsWorker(start, end, url);
+    //     return result;
+    // };
 
     return (
         <div>
-            <p>WILL LOAD DATA</p>
+            <p>QRYN Raggix</p>
+            <label>recurrent: </label>{" "}
+            <Switch
+                checked={isRecurrent}
+                size={"small"}
+                onChange={handleRecurrent}
+                inputProps={{ "aria-label": "controlled" }}
+            />{" "}
+            <button onClick={handleReset}>reset</button>
             {/* {logs} */}
-            {/* {logs.map((log: any, idx: number) => (
-                <div key={idx}>{log}</div>
-            ))} */}
+
+            <label>Open Logs: </label>{" "}
+            <Switch
+                checked={open}
+                size={"small"}
+                onChange={openLogs}
+                inputProps={{ "aria-label": "controlled" }}
+            />
+
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {logs &&
+                    logs?.map((log: any, idx: number) => (
+                        <div key={idx}>
+                            <LogItem log={log} openLog={openLog} index={idx} />
+                        </div>
+                    ))}
+
+            </div>
+            <div>
+            {open && logs?.length > 0 && (
+                    <LogsList logs={logs[index]}/>
+                )}
+
+            </div>
         </div>
     );
 };
+
+
+const levelColors: any = {
+    error: "red",
+    info: "green",
+    information: "green",
+    warn: "purple",
+    warning: "purple",
+    debug: "blue",
+};
+
+export const LogItem = (props: any) => {
+    const {openLog, index} = props
+    console.log(props.log);
+    const level = useMemo(() => {
+        if(props?.log?.stream?.level) {
+            const keys = Object.keys(props.log.stream);
+            if (keys.includes("level")) {
+                return levelColors[props.log.stream.level|| "info"];
+            }
+        }
+
+        return "";
+    }, [props.log.stream]);
+
+    console.log(level);
+    return (
+        <Tooltip title={JSON.stringify(props.log.stream)}>
+            <div
+                onClick={()=>openLog(index)}
+                style={{
+                    height: "20px",
+                    width: "20px",
+                    background: level || "gray",
+                    margin: "3px",
+                    borderRadius: "3px",
+                }}
+            >
+
+          
+
+            </div>
+        </Tooltip>
+    );
+};
+
+export const LogsList = (props:any) =>{ 
+    const {logs} = props 
+    console.log(logs)
+
+    return (
+        <div>
+            {logs?.values?.length > 0  && logs?.values?.map((log:any,idx:any)=>{
+                const [ts,text] = log
+                return<div style={{fontSize:'12px',fontFamily:"monospace",display:'flex'}} key={idx}>
+                  <span style={{margin:'2px'}}>{ts}</span> <span style={{margin:'2px'}}>{text}</span>
+                </div>
+            })}
+        </div>
+    )
+}
 
 export default Raggix;
