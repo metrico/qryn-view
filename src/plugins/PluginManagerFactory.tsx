9ff5b138d53f75ea660cded4ca4841357afd9b35
jacovinus <jacovinus@gmail.com>
2023-04-06 10:56:32 +0200

    fix: manage local

--- src/plugins/PluginManagerFactory.tsx
+++ src/plugins/PluginManagerFactory.tsx
@@ -1,7 +1,6 @@
 import store from "../store/store";
 import { setPlugins } from "./actions";
 
-
 export interface ILocation {
     parent: string;
     children: string[];
@@ -30,15 +29,91 @@ export const locations: ILocations = {
         parent: "Panel",
         children: ["Data View Header", "View"],
     },
-    "Query Item" :{
-            parent: "Queries",
-            children:[]
-    }
+    "Query Item": {
+        parent: "Queries",
+        children: [],
+    },
 };
 
+export function LocalPluginsManagement() {
+    function getAll() {
+        try {
+            return JSON.parse(localStorage.getItem("plugins") || "{}");
+        } catch (e) {
+            return {};
+        }
+    }
+
+    function setPlugin(location: string, plugin: any) {
+        let plugins = getAll();
+
+        if (!plugins[location]) {
+            plugins[location] = [];
+        }
+
+        if (!plugins[location]?.some((s: any) => s.name === plugin.name)) {
+            plugins[location].push(plugin);
+            localStorage.setItem("plugins", JSON.stringify(plugins));
+        }
+    }
+
+    function getPluginsFromLocation(location: string) {
+        let plugins = getAll();
+        if (!plugins[location]) {
+            return [];
+        }
+        return plugins[location];
+    }
+
+    function removePlugin(location: string, name: string) {
+        let plugins = getAll();
+
+        if (
+            plugins[location] &&
+            Array.isArray(plugins[location]) &&
+            plugins[location]?.some((s: any) => s.name === name)
+        ) {
+            let filtered = plugins[location]?.filter(
+                (f: any) => f.name !== name
+            );
+            plugins[location] = filtered;
+            localStorage.setItem("plugins", JSON.stringify(plugins));
+        }
+    }
+
+    function togglePlugin(location: string, name: string, active: boolean) {
+        const plugins = getAll();
+        const pluginIndex = plugins[location]?.findIndex(
+            (pl: any) => pl?.name === name
+        );
+
+        if (pluginIndex >= 0) {
+            const updatedPlugins = {
+                ...plugins,
+                [location]: plugins[location].map((pl: any, index: number) =>
+                    index === pluginIndex ? { ...pl, active } : pl
+                ),
+            };
+
+            localStorage.setItem("plugins", JSON.stringify(updatedPlugins));
+        }
+    }
+
+    return {
+        getAll,
+        getPluginsFromLocation,
+        setPlugin,
+        removePlugin,
+        togglePlugin,
+    };
+}
+
 export function PluginManagerFactory(locations: ILocations) {
-    const plugins: any = store.getState().plugins || {};
-   // const dispatch = store.dispatch
+    const plugins: any = {};
+    const lp = LocalPluginsManagement();
+    let localPlugins = lp.getAll();
+
+    // const dispatch = store.dispatch
 
     // add plugin to a specific location
     function registerPlugin(plugin: any) {
@@ -48,10 +123,17 @@ export function PluginManagerFactory(locations: ILocations) {
 
         plugins[plugin.section].push(plugin);
 
-        store.dispatch(setPlugins(plugins))
+        store.dispatch(setPlugins(plugins));
 
-        localStorage.setItem("plugins", JSON.stringify(plugins));
+        if (
+            !localPlugins[plugin.section]?.some(
+                (s: any) => s.name === plugin.name
+            )
+        ) {
+            lp.setPlugin(plugin.section, plugin);
+        }
     }
+
     function registerPluginGlobally(plugin: any) {
         for (let location in locations) {
             if (location !== "Main") {
@@ -61,6 +143,7 @@ export function PluginManagerFactory(locations: ILocations) {
     }
 
     function getPlugins(location: string) {
+        let local = lp.getPluginsFromLocation(location);
         const unique = plugins?.[location]?.filter(
             (obj: any, index: number) =>
                 plugins[location]?.findIndex(
@@ -68,7 +151,22 @@ export function PluginManagerFactory(locations: ILocations) {
                 ) === index
         );
 
-        return unique || [];
+        let active = [];
+
+        if (unique?.length > 0) {
+            for (let lp of local) {
+                let compared = unique?.find?.((f: any) => f.name === lp.name);
+                if (lp.active) {
+                    active.push(compared);
+                }
+            }
+        }
+
+        return active || [];
+    }
+
+    function togglePlugin(location: string, name: string, active: boolean) {
+        lp.togglePlugin(location, name, active);
     }
 
     function getAllPlugins() {
@@ -87,6 +185,7 @@ export function PluginManagerFactory(locations: ILocations) {
         registerPluginGlobally,
         getAllPlugins,
         getPlugins,
+        togglePlugin,
     };
 }
 
