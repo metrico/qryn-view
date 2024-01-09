5e27c1a61f70df6ad1d605be35507f26098dab2b
jacovinus <jacovinus@gmail.com>
2024-01-09 16:20:51 +0000

    Deploying to gh-pages from @ metrico/qryn-view@838d9b8d999c25f9e2920dd860df8657d9dc1933 🚀

--- assets/reactDnd-441585ba.js
+++ assets/reactDnd-441585ba.js
@@ -1,4 +1,4 @@
-import{r as h,g as He}from"./react-432945ee.js";import{_ as je}from"./vendor-918e6b38.js";var pe={exports:{}},R={};/**
+import{r as h,g as He}from"./react-432945ee.js";import{_ as je}from"./vendor-92926b2c.js";var pe={exports:{}},R={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
